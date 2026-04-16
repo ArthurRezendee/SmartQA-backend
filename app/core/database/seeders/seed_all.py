85e12c1728366766b2abe462949d942597d7359a
Arthur Rezende <arezendealmeida@gmail.com>
2026-04-16 04:00:22 -0300

    fix

--- app/core/database/seeders/seed_all.py
+++ app/core/database/seeders/seed_all.py
@@ -5,9 +5,6 @@ from app.modules.billing.model.billing_account_model import BillingAccount
 from app.modules.organization.model.organization_model import Organization
 from app.modules.user.model.user_model import User
 from app.modules.organization.model.organization_member_model import OrganizationMember
-from app.modules.qa_analysis.model.qa_analysis_model import QaAnalysis
-from app.modules.qa_analysis.model.qa_document_model import QaDocument
-from app.modules.qa_analysis.model.access_credential_model import AccessCredential
 from app.modules.test_case.model.test_case_model import TestCase
 from app.modules.test_case.model.test_case_step_model import TestCaseStep
 from app.modules.user.model.process_notification_model import ProcessNotification
